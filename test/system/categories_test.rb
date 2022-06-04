2f1c5a362a19827102881459cd65e78df04a4a61
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-04 18:27:32 +0200

    Update Title With Translation

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class CategoriesTest < ApplicationSystemTestCase
   setup do
-    @category = categories(:one)
+    @category = categories(:autos)
   end
 
   test "visiting the index" do
@@ -14,28 +14,21 @@ class CategoriesTest < ApplicationSystemTestCase
     visit categories_url
     click_on "New category"
 
-    fill_in "Name", with: @category.name
+    fill_in "Name", with: 'Chiki'
     click_on "Create Category"
 
     assert_text "Category was successfully created"
-    click_on "Back"
+
   end
 
   test "should update Category" do
-    visit category_url(@category)
-    click_on "Edit this category", match: :first
+    visit categories_url(@category)
+    click_on "Edit", match: :first
 
-    fill_in "Name", with: @category.name
+    fill_in "Name", with: 'Updatedname ' + @category.name
     click_on "Update Category"
 
     assert_text "Category was successfully updated"
-    click_on "Back"
   end
 
-  test "should destroy Category" do
-    visit category_url(@category)
-    click_on "Destroy this category", match: :first
-
-    assert_text "Category was successfully destroyed"
-  end
 end
