8955c4519324aa0cf4984457297deaf75883aebd
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-04 13:07:59 +0200

    Refactor Products Controller => Query Object Pattern

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,49 +1,37 @@
 class ProductsController < ApplicationController
-  before_action :product, only: [:show,:edit,:destroy, :update]
+  before_action :product, only: %i[show edit destroy update]
   def index
-
     @categories = Category.order(name: :asc).load_async
-    @products = Product.all.with_attached_main_photo
-    if params[:category_id]
-      @products = @products.where(category_id: params[:category_id])
-    end
-    if params[:min_price].present? 
-      @products = @products.where("price >= ?", params[:min_price])
-    end
-    if params[:max_price].present?
-      @products = @products.where("price <= ?", params[:max_price])
-    end
-    if params[:query_text].present?
-      @products = @products.search_full_text(params[:query_text])
-    end
-    order_in_params = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
-    @products = @products.order(order_in_params).load_async
-
-    @pagy, @products = pagy_countless(@products, items: 5)
+    # @products = Product.with_attached_main_photo
+    # @products = @products.where(category_id: params[:category_id]) if params[:category_id]
+    # @products = @products.where('price >= ?', params[:min_price]) if params[:min_price].present?
+    # @products = @products.where('price <= ?', params[:max_price]) if params[:max_price].present?
+    # @products = @products.search_full_text(params[:query_text]) if params[:query_text].present?
+    # order_in_params = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
+    # @products = @products.order(order_in_params).load_async
+      
+    @pagy, @products = pagy_countless(FindProducts.new.call(params).load_async, items: 5)
   end
 
-  def show
-  end
+  def show; end
 
   def new
-    @product = Product.new 
+    @product = Product.new
   end
 
   def create
     @product = Product.new(product_params)
-    
-    if @product.save 
+
+    if @product.save
       redirect_to products_path, notice: t('.notice')
     else
       render :new, status: :unprocessable_entity, alert: t('.alert')
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
-
     if @product.update(product_params)
       redirect_to products_path, notice: t('.notice')
     else
@@ -66,5 +54,4 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:title, :description, :price, :main_photo, :category_id)
   end
-
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,21 @@
 class Product < ApplicationRecord
-    include PgSearch::Model
-    pg_search_scope :search_full_text, against: {
-        title: 'A',
-        description: 'B'
-      }
-    has_one_attached :main_photo
+  include PgSearch::Model
+  pg_search_scope :search_full_text, against: {
+    title: 'A',
+    description: 'B'
+  }
+  has_one_attached :main_photo
 
-    ORDER_BY = {
-      newest: "created_at DESC",
-      expensive: "price DESC",
-      cheapest: "price ASC"
-    }
-    
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :price, presence: true
-    # validates :main_photo, presence: true
+  ORDER_BY = {
+    newest: 'created_at DESC',
+    expensive: 'price DESC',
+    cheapest: 'price ASC'
+  }
 
-    belongs_to :category
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  # validates :main_photo, presence: true
+
+  belongs_to :category
 end
