17153f09e305fc915fd1e546d15895d96f64dc33
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-10 00:28:25 +0200

    Add sessions controller

---
+++ test/controllers/authentication/sessions_controller_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class Authentication::SessionsControllerTest < ActionDispatch::IntegrationTest
+    def setup
+        @user = users(:john)
+    end
+  test "should get new" do
+    get new_session_url
+    assert_response :success
+  end
+
+  test "should create login user by email" do
+    
+    post sessions_url, params: { login: @user.email, password: 'testme' }
+      
+    assert_redirected_to products_url
+  end 
+
+  test "should create login user by username" do
+    
+    post sessions_url, params: { login: @user.username, password: 'testme' }
+      
+    assert_redirected_to products_url
+  end 
+end
