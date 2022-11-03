51a515d6e31677108dd45084d02a7ee74f434d25
Martin Arce <martin.arce.teixeira@gmail.com>
2022-11-03 08:25:13 +0100

    User mailer

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -1,21 +1,23 @@
 class Authentication::UsersController < ApplicationController
-    skip_before_action :protect_pages
-    def new
-        @user = User.new
-    end
+  skip_before_action :protect_pages
+  def new
+    @user = User.new
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to products_path, notice: t('.created')
-        else
-            render :new, status: :unprocessable_entity
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      UserMailer.with(user: @user).welcome.deliver_later
+      session[:user_id] = @user.id
+      redirect_to products_path, notice: t('.created')
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    private
-    def user_params
-        params.require(:user).permit(:email,:username,:password)
-    end
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
+  end
 end
